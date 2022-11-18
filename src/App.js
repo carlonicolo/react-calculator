fc8a5ee156c7a46f6fe7708d689fe89de664c736
Carlo <carlo.nicolo@hotmail.com>
2022-11-18 23:27:29 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
     if (firstNumber === "0") {
       setFirstNumber(String(currentNumber));
       setCurrentNumber("0");
-      setOperation("+");
+      setOperation("x");
     } else {
       const mult = Number(firstNumber) * Number(currentNumber);
       setCurrentNumber(String(mult));
